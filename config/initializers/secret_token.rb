8bc4bd996d8c091027ff021063940e508237700d
rennew <reneenewton@new-host-2.home>
2013-12-01 17:34:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecogagement::Application.config.secret_token = 'cb43f278785cf7156713913df27118cffae34fb3be1339b5263a95d15ba6d3c319b00545ffc49b9a315a5c4687de6847450f6253459002284acf2e8bc4573c20'
